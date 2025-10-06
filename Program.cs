9189f8b251e1761c8898fc90f391bb3847c85a90
XD0uc3tt3 <doucettexander@gmail.com>
2025-10-06 09:45:23 -0300

    Implemented Framework of the project

--- Program.cs
+++ Program.cs
@@ -8,9 +8,67 @@ namespace HealthSystem
 {
     internal class Program
     {
+
+        static int maxHealth = 100;
+        static int health = maxHealth;
+
+        static int maxShield = 100;
+        static int shield = maxShield;
+
+        static int lives = 3;
         static void Main(string[] args)
         {
+            ShowHud();
 
+        }
+
+        static void ShowHud()
+        {
+            string characterName = "Brutus Jr";
+
+
+            //Character Name
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("{0,0}", characterName);
+
+            //Health Status
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            if (health == 100)
+            {
+                Console.Write("{0,25}", "Perfect Health");
+            }
+            else if (health >= 75)
+            {
+                Console.Write("{0,25}", "Healthy");
+            }
+            else if (health >= 50 && health <= 75)
+            {
+                Console.Write("{0,25}", "Hurt");
+            }
+            else if (health >= 10 && health <= 50)
+            {
+                Console.Write("{0,25}", "Badly Hurt");
+            }
+            else if (health >= 0 && health <= 10)
+            {
+                Console.Write("{0,25}", "Immeniate Danger");
+            }
+            else
+            {
+                Console.Write("{0,65}", "InValid Health Status");
+            }
+
+            //Health
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("{0,60}", "Health:" + health + "/" + maxHealth);
+
+            //Weapon
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("{0,17}", "Sheild:" + shield + "/" + maxShield);
+
+            //Score
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("{0,10}", "Lives:" + lives);
 
         }
     }
